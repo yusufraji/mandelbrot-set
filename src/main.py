ff6cb672b1461e26ca07fd862ca7347ba5deeaad
Yusuf Raji <raji.yusuf234@gmail.com>
2022-02-01 20:57:44 +0800

    Save julia set to mp4 & gif

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+from importlib.metadata import metadata
 from pathlib import Path
 
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
 import streamlit as st
 
 import utils
@@ -20,10 +24,51 @@ if set_type == "Julia Set":
     frame_text = st.empty()
     st_fig = st.empty()
 
-    utils.julia(progress_bar, frame_text, separation, iterations, st_fig)
+    _ = utils.julia(progress_bar, frame_text, separation, iterations, st_fig)
 
     # We clear elements by calling empty on them.
     progress_bar.empty()
     frame_text.empty()
 
-    st.button("Re-run")
+    frames = 100  # number of animation frames
+    m, n, s = 960, 640, 400
+    x = np.linspace(-m / s, m / s, num=m).reshape((1, m))  # real axis
+    y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))  # imaginary axis
+    fig = plt.figure(figsize=(10, 10))
+    ax = plt.axes()
+    anim = animation.FuncAnimation(
+        fig,
+        utils.animate_julia,
+        fargs=(
+            ax,
+            x,
+            y,
+            separation,
+            iterations,
+        ),
+        frames=100,
+        interval=50,
+        blit=True,
+    )
+
+    video_writer = animation.FFMpegWriter(fps=5)
+    gif_writer = animation.PillowWriter(fps=5)
+    anim.save(str(Path.cwd() / "graphics" / "julia_set.mp4"), writer=video_writer)
+    anim.save(str(Path.cwd() / "graphics" / "julia_set.gif"), writer=gif_writer)
+
+    st.write(
+        "This render might be slow, when it's done, consider downloading the gif or mp4 to view locally."
+    )
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.button("Re-run")
+    with col2:
+        with open(Path.cwd() / "graphics" / "julia_set.gif", "rb") as f:
+            st.download_button(
+                "Download as gif", data=f, file_name="julia_set.gif", mime="image/gif"
+            )
+    with col3:
+        with open(Path.cwd() / "graphics" / "julia_set.mp4", "rb") as f:
+            st.download_button(
+                "Download as mp4", data=f, file_name="julia_set.mp4", mime="image/mp4"
+            )
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,3 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -34,3 +33,26 @@ def julia(progress_bar, frame_text, separation, iterations, st_fig):
         ax.imshow(1.0 - (N / N.max()), interpolation="hanning", cmap="magma_r")
         # Update the fig placeholder by calling the pyplot() function on it.
         st_fig.pyplot(fig, cmap="magma", clear_figure=True)
+
+
+def animate_julia(a, ax, x, y, separation, iterations):
+    a = a * (4 * np.pi) / 100
+    ax.clear()
+    ax.set_xticks([], [])
+    ax.set_yticks([], [])
+    m, n, s = 960, 640, 400
+    # Performing some fractal wizardry.
+    c = separation * np.exp(1j * a)
+    Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
+    C = np.full((n, m), c)
+    M: Any = np.full((n, m), True, dtype=bool)
+    N = np.zeros((n, m))
+
+    for i in range(iterations):
+        Z[M] = Z[M] * Z[M] + C[M]
+        M[np.abs(Z) > 2] = False
+        N[M] = i
+
+    img = ax.imshow(1.0 - (N / N.max()), interpolation="hanning", cmap="magma_r")
+
+    return [img]
